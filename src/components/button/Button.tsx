4bb7c231ef290b813f4f3cdb98ddad77dde7a3b6
Kevin Stark <kstark734@gmail.com>
2023-02-11 16:12:39 +0000

    start creating button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,28 @@
-type Props = {
-  name: string;
-  address: string;
+type BtnProps = {
+  variant: string;
+  disabled: boolean;
+  children: React.ReactNode;
 };
 
-function Button({name, address}: Props) {
+type StyleType = {
+  backgroundColor: string;
+  padding: string;
+};
+
+function Button({variant, disabled, children}: BtnProps) {
+  const primaryStyle: StyleType = {
+    backgroundColor: 'blue',
+    padding: '20px 40px',
+  };
+  const secondaryStyle = {};
   return (
-    <div>
-      {name} {address}
-    </div>
+    <button
+      style={variant === 'primary' ? primaryStyle : secondaryStyle}
+      disabled={disabled}
+      type="button"
+    >
+      {children}
+    </button>
   );
 }
 
