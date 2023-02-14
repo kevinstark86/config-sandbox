9a823801f4d51aeff9c4d8602c788e0d39deb067
Kevin Stark <kstark734@gmail.com>
2023-02-14 23:50:04 +0000

    add styling props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,25 +2,17 @@
 import styled from 'styled-components';
 import {BtnProps} from './types';
 
-const Wrapper = styled.button`
+const Wrapper = styled.button<BtnProps>`
   padding: 10px 40px;
   border: none;
   border-radius: 5px;
-
+  background-color: ${props => (props.variant === 'primary' ? 'blue' : 'red')};
   color: ivory;
-  .primary {
-    background-color: lightblue;
-    color: ivory;
-  }
-  .secondary {
-    background-color: orange;
-    color: ivory;
-  }
 `;
 
 function Button({variant, disabled, children}: BtnProps): JSX.Element {
   return (
-    <Wrapper className={variant} disabled={disabled} type="button">
+    <Wrapper variant={variant} disabled={disabled} type="button">
       {children}
     </Wrapper>
   );
